6bb80877aa4597ec2c1b554e59c29a44e01e5422
Andri <andri@bizoftenterprise.com>
2022-08-02 12:37:56 +0700

    change gruvbox lua

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,2 +1,8 @@
-vim.g.gruvbox_contrast_dark = "hard"
+-- vim.g.gruvbox_contrast_dark = "hard"
+vim.o.background = "dark"
+require("gruvbox").setup({
+    underline = false,
+    italic = false,
+    contrast = "hard",
+})
 vim.cmd "colorscheme gruvbox"
